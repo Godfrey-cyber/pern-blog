998d8b0dd40edc720c4214be03b68c6a3f4531b6
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-08-29 08:04:30 +0300

    added a category route

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { createCategory } from "../controllers/category.js";
-import { authenticate, restrictTo, } from '../middleware/middleware.js'
+import { authenticate, restrictTo, categories } from '../middleware/middleware.js'
 
 router.post('/create-category', authenticate, restrictTo('ADMIN'), createCategory);
-// router.get('/get-categories', authenticate, restrictTo('ADMIN'), getCategories);
+router.get('/get-categories', authenticate, categories);
 
 export default router;
