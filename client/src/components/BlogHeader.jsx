78fa09cccd1816faae39663d32df16780b7c36f8
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-09-04 13:17:52 +0300

    added a header - basic functionality

--- client/src/components/BlogHeader.jsx
+++ client/src/components/BlogHeader.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { MdOutlineChevronLeft, MdOutlineChevronRight } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
+import { IoMdGlobe, IoMdSearch } from "react-icons/io";
+import { useNavigate, Link } from 'react-router-dom';
 
 const BlogHeader = () => {
 	const navigate = useNavigate();
 	return (
-		<nav className="w-full h-20 px-5 md:px-10 lg:px-20 bg-gray-200 flex flex-row justify-between items-center">
-			<span className="flex flex-col items-center justify-center">
-				<img src="https://www.shutterstock.com/image-vector/blog-writing-line-icon-web-600nw-2366232875.jpg" alt="" className="h-12 w-12 object-cover" />
+		<nav className="navbar">
+		<Link to="/">
+			<span className="flex flex-col items-center">
+				<img src="https://www.shutterstock.com/image-vector/blog-writing-line-icon-web-600nw-2366232875.jpg" alt="" className="h-18 w-18 object-cover" />
 			</span>
-			<div className="flex justify-between w-full">
-				<p className="text-sm font-semibold text-red-400">Blog</p>
-				<p className="text-sm font-semibold text-red-400">Blog</p>
+		</Link>
+			<div className="flex space-x-3 items-center">
+				<p className="nav-list">MARKET</p>
+				<p className="nav-list">LEADERS</p>
+				<p className="nav-list">CAREERS</p>
+				<p className="nav-list">LIFESTYLE</p>
+			</div>
+			<div className="flex space-x-5 items-center">
+				<IoMdSearch className="nav-icon" /> 
+				<IoMdGlobe className="nav-icon" /> 
 			</div>
 		</nav>
 	)
